d982a2ce20481e563d5811e20de33c5bc2de3661
Chansik Yun <chansigi@gmail.com>
2024-12-18 08:48:04 +0900

    .

--- 1_생성자.cpp
+++ 1_생성자.cpp
@@ -5,11 +5,18 @@ using namespace std;
 class Base {
 public:
     Base() { cout << "Base()" << endl; }
+    Base(int) { cout << "Base(int)" << endl; }
     ~Base() { cout << "~Base()" << endl; }
 };
 
 class Derived : public Base {
 public:
+    Derived(int n)
+        : Base { n } // 부모의 기본생성자가 아닌 경우, 명시적으로 초기화리스트를 통해 작성해주어야 합니다.
+    {
+        cout << "Derived(int)" << endl;
+    }
+
     Derived()
     // : Base()
     // 2) 위의 코드를 컴파일러가 생성합니다.
@@ -28,6 +35,8 @@ public:
 
 int main()
 {
-    Derived d; // 1) Derived::Derived()
+    Derived d { 42 };
+
+    // Derived d; // 1) Derived::Derived()
 }
 // 1> Derived::~Derived();
