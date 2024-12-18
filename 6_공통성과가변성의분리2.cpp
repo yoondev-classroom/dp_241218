3ed803bc5fad1f46405a0009f55579d0eade1238
Chansik Yun <chansigi@gmail.com>
2024-12-18 11:28:34 +0900

    .

--- 6_공통성과가변성의분리2.cpp
+++ 6_공통성과가변성의분리2.cpp
@@ -25,6 +25,11 @@ int main()
 }
 #endif
 
+// 변하는 것과 변하지 않는 것은 분리되어야 합니다.
+// => "공통성과 가변성의 분리"
+//  1) 변하는 정책을 가상함수를 통해 분리하고,
+//     자식 클래스를 통해 재정의합니다. - Template Method Pattern
+
 class EditBox {
     string data;
 
@@ -39,7 +44,7 @@ public:
                 break;
             }
 
-            if (isdigit(ch)) {
+            if (isdigit(ch)) { // 정책은 변할 수 있습니다.
                 data.push_back(ch);
                 cout << ch;
             }
