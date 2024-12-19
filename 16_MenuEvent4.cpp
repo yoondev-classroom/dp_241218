d8731b928e24c7388b8b29a256d020932d55eacf
Chansik Yun <chansigi@gmail.com>
2024-12-19 14:30:11 +0900

    .

--- 16_MenuEvent4.cpp
+++ 16_MenuEvent4.cpp
@@ -31,8 +31,52 @@ public:
 
 void foo() { cout << "foo" << endl; }
 
+// 2. 멤버 함수 포인터의 역활을 수행하는 클래스
+class Dialog {
+public:
+    void Close() { cout << "Dialog::Close" << endl; }
+};
+
+template <typename TYPE>
+class MemberCommand : public ICommand {
+    TYPE* obj;
+    void (TYPE::*handler)();
+
+public:
+    MemberCommand(TYPE* p, void (TYPE::*h)())
+    {
+        obj = p;
+        handler = h;
+    }
+
+    void Call() override
+    {
+        (obj->*handler)(); // !!!
+    }
+};
+
+int main()
+{
+    Dialog dlg;
+
+    MemberCommand mc { &dlg, &Dialog::Close };
+    mc.Call();
+}
+
+#if 0
+int main()
+{
+    Dialog dlg;
+    void (Dialog::*fp)() = &Dialog::Close;
+
+    (dlg.*fp)();
+}
+#endif
+
+#if 0
 int main()
 {
     FunctionCommand fc { &foo };
     fc.Call();
 }
+#endif
